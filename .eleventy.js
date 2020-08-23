52a5ea0be4164344fbaf6cdb204617f290410254
Luce Carter <luce@lucyscarter.co.uk>
2020-08-23 15:58:15 +0100

    Added in css stylesheet and custom font

--- .eleventy.js
+++ .eleventy.js
@@ -15,7 +15,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
 
   // Folders to copy to output folder
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("Styles");
+
+  eleventyConfig.addPassthroughCopy("Images");
 };
 
 function extractExcerpt(article) {
