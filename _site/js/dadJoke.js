89f2075f6ac9973bd3811c0b7feb9f38916f16b5
Luce Carter <luce@lucyscarter.co.uk>
2020-08-28 14:46:22 +0100

    Finalised DadJoke js

--- _site/js/dadJoke.js
+++ _site/js/dadJoke.js
@@ -1,8 +1,8 @@
-function getDadJoke() {
-  console.log("Fetch our hourly Dad joke... ");
+ console.log("Fetching a Dad joke... ");
 
   const myHeaders = new Headers();
   myHeaders.append("Accept", "text/plain");
+  myHeaders.append("User-Agent", "CodingWithLuceBlog");
   myHeaders.append(
     "Cookie",
     "__cfduid=d0222b48113a0cf0066ef0a9dd6572a2e1598302507"
@@ -21,10 +21,3 @@ function getDadJoke() {
       document.getElementById("dadJoke").innerText = result;
     })
     .catch((error) => console.log("error", error));
-
-  }
-
-  var currentJoke =   getDadJoke();
-
-
-  setInterval(getDadJoke, 3600000);
